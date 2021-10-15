13270c1bfbba6230f5948cb449c70a6e4b5e0c2a
Raikonif <raikonif@gmail.com>
2021-10-15 12:47:44 -0400

    MoviesDetail and Movie Fragments xml designed

---
+++ app/src/main/java/com/example/moviesapp/MovieDetailFragment.kt
@@ -0,0 +1,13 @@
+package com.example.moviesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MovieDetailFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
