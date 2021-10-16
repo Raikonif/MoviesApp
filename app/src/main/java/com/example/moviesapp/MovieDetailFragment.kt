5f29397466a23ac2f08b9b193f732a55e385309d
Raikonif <raikonif@gmail.com>
2021-10-16 15:15:35 -0400

    MovieFragment, Movie DetailFragment modified

--- app/src/main/java/com/example/moviesapp/MovieDetailFragment.kt
+++ app/src/main/java/com/example/moviesapp/MovieDetailFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.moviesapp.databinding.FragmentMovieDetailBinding
 
-class MovieDetailFragment : Fragment() {
+class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+
+    private val binding: FragmentMovieDetailBinding by lazy {
+        FragmentMovieDetailBinding.inflate(layoutInflater)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        FragmentMovieDetailBinding.bind(view)
     }
 }
--- app/src/main/java/com/example/moviesapp/MovieFragment.kt
+++ app/src/main/java/com/example/moviesapp/MovieFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.moviesapp.databinding.FragmentMovieBinding
 
 
-class MovieFragment : Fragment() {
+class MovieFragment : Fragment(R.layout.fragment_movie) {
+
+    private val binding: FragmentMovieBinding by lazy { FragmentMovieBinding.inflate(layoutInflater) }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
