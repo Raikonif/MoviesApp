5f7cdca50b8a1d89da199181ac100a8173f14d00
Raikonif <raikonif@gmail.com>
2021-10-19 16:51:58 -0400

    added data class Credits

---
+++ app/src/main/java/com/example/moviesapp/data/model/Credits.kt
@@ -0,0 +1,16 @@
+package com.example.moviesapp.data.model
+
+data class Credits(
+    val adult: Boolean = false,
+    val gender: Int = -1,
+    val id: Int = -1,
+    val known_for_department: String = "",
+    val name: String = "",
+    val original_name: String = "",
+    val popularity: Double = 1.815,
+    val profile_path: String = "",
+    val cast_id: Int = 4,
+    val character: String = "",
+    val credit_id: String = "",
+    val order: Int = 2
+)
