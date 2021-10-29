a0bfd9c23b1e98c39d28385daf731ccf1d9d5812
Raikonif <raikonif@gmail.com>
2021-10-29 12:06:29 -0400

    created MovieDao

--- app/src/main/java/com/example/moviesapp/data/model/Movie.kt
+++ app/src/main/java/com/example/moviesapp/data/model/Movie.kt
@@ -1,5 +1,8 @@
 package com.example.moviesapp.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 data class Movie(
@@ -22,4 +25,36 @@ data class Movie(
 
 data class MovieList(
     val results:List<Movie> = listOf()
+)
+
+//Room
+@Entity
+data class MovieEntity(
+    @PrimaryKey
+    val id: Int = -1,
+    @ColumnInfo(name = "adult")
+    val adult: Boolean = false,
+//    val gender_ids: List<Int> = listOf(),
+    @ColumnInfo(name = "backdrop_path")
+    val backdrop_path: String = "",
+    @ColumnInfo(name = "original_title")
+    val original_title: String = "",
+    @ColumnInfo(name = "original_language")
+    val original_language: String = "",
+    @ColumnInfo(name = "overview")
+    val overview: String = "",
+    @ColumnInfo(name = "popularity")
+    val popularity: Double = -1.0,
+    @ColumnInfo(name = "poster_path")
+    val poster_path: String = "",
+    @ColumnInfo(name = "release_date")
+    val release_date: String = "",
+    @ColumnInfo(name = "title")
+    val title: String = "",
+    @ColumnInfo(name = "video")
+    val video: Boolean = false,
+    @ColumnInfo(name = "vote_average")
+    val vote_average: Double = -1.0,
+    @ColumnInfo(name = "vote_count")
+    val vote_count: Int = -1
 )
