cdbf167ad92af659ce3c710c36c07616049b8e06
Raikonif <raikonif@gmail.com>
2021-11-07 18:40:09 -0400

    update

--- app/src/main/java/com/example/moviesapp/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/moviesapp/repository/MovieRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package com.example.moviesapp.repository
 
+import com.example.moviesapp.data.local.LocalMovieDataSource
 import com.example.moviesapp.data.model.MovieList
 import com.example.moviesapp.data.remote.RemoteMovieDataSource
 
-class MovieRepositoryImpl(private val dataSourceRemote: RemoteMovieDataSource): MovieRepository {
+class MovieRepositoryImpl(
+    private val dataSourceRemote: RemoteMovieDataSource,
+    private val dataSourceLocal: LocalMovieDataSource
+) : MovieRepository {
     override suspend fun getUpcomingMovies():
             MovieList = dataSourceRemote.getUpcomingMovies()
 
